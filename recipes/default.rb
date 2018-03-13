1cf572d9ca2f9704fa66bca5eeb30c11a7a8b029
thegov18 <thegov18@gmail.com>
2018-03-13 19:39:17 -0400

    Initial Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+    'hostname' => 'ec2-52-87-182-143.compute-1.amazonaws.com',
+    'ipaddress' => '52.87.182.143',
+    'port' => 80,
+    'ssl_port' => 80
+    }]
+
+include_recipe 'haproxy::manual'
