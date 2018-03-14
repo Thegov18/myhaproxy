a29efd10f1e597679f1cb5c6ddcb9bf69445c8ea
thegov18 <thegov18@gmail.com>
2018-03-14 13:59:59 -0400

    Version 1.0.0, dynamic nodes

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,17 +4,20 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [{
-    'hostname' => 'ec2-52-87-182-143.compute-1.amazonaws.com',
-    'ipaddress' => '52.87.182.143',
+all_web_nodes = search('node','role:web_server')
+
+members = []
+
+all_web_nodes.each do |web_node|
+  member = {
+    'hostname' => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
     'port' => 80,
     'ssl_port' => 80
-    },{
-        'hostname' => 'ec2-52-91-6-3.compute-1.amazonaws.com',
-        'ipaddress' => '52.91.6.3',
-        'port' => 80,
-        'ssl_port' => 80
-        }
-    ]
+ }
+ members.push(member)
+end
+
+node.default['haproxy']['members'] = members
 
 include_recipe 'haproxy::manual'
