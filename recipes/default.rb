d21a5f32989ee56875fc2e61a3c671b61d7452fe
thegov18 <thegov18@gmail.com>
2018-03-14 10:31:07 -0400

    Added apache memeber

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,12 @@ node.default['haproxy']['members'] = [{
     'ipaddress' => '52.87.182.143',
     'port' => 80,
     'ssl_port' => 80
-    }]
+    },{
+        'hostname' => 'ec2-52-91-6-3.compute-1.amazonaws.com',
+        'ipaddress' => '52.91.6.3',
+        'port' => 80,
+        'ssl_port' => 80
+        }
+    ]
 
 include_recipe 'haproxy::manual'
